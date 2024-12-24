6febcaa7c6ad70e6035dcf5e6e37f7349fb0f84b
David Warmuth <info@davidwarmuth.de>
2024-12-24 14:00:33 +0100

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Header } from "@/components/header";
+import { cn } from "@/lib/utils";
+import Footer from "@/components/footer";
+import Providers from "@/components/providers";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "NSC Template",
+  description: "NextJs template with shadcn/ui and Clerk authentication",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
+      <body
+        className={cn(
+          geistSans.variable,
+          geistMono.variable,
+          "antialiased min-h-screen flex flex-col"
+        )}
+      >
+        <Providers>
+          <Header />
+          <main className="grow">{children}</main>
+          <Footer />
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3xl font-bold">NextJS Shadcn Clerk Starter</h1>
+        <p className="mt-3 text-muted-foreground">
+          A NextJS starter template with Clerk authentication, shadcn/ui and
+          Tailwind CSS.
+        </p>
+      </div>
+    </section>
+  );
+}
